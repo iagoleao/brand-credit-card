0e0b7ac10ce7d2db9afc3a0fd281c59432514185
Iago <iago.leao90@gmail.com>
2017-08-08 23:50:04 -0300

    First Commit

---
+++ tests/BCC/BrancCreditCardTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BCC;
+
+use BCC\BrandCreditCard;
+
+class BrandCreditCardTest extends \PHPUnit\Framework\TestCase
+{
+    public function provider()
+    {
+        return array(
+          array('visa', '4539778873341120'),
+          array('master', '5533093008993479'),
+          array('amex', '341607632839486'),
+          array('dinners', '30344401017765'),
+          array('hipercard', '6062820640453968'),
+          array('elo', '6362970000457013'),
+          array('aura', '5078601870000127985')
+        );
+    }
+
+    /**
+     * @dataProvider provider
+     */ 
+    public function testBrand($brand, $number) {
+        $this->assertEquals($brand, BrandCreditCard::getBrand($number));
+    }
+}
